9c4267e9dc8153968b888de1da96361da50e826c
itsXILO <scienceunveiled1@gmail.com>
2026-07-25 19:01:23 +0530

    initial-setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Express!" });
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
